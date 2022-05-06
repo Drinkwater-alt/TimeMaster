055ae73c608dac027ff8e2f84a6fbbefe438dc3f
Ethan-Yuanxy <947253705@qq.com>
2022-05-06 19:54:01 +0800

    修改navbar bug

--- src/App.vue
+++ src/App.vue
@@ -32,10 +32,10 @@
     height: 1.5em;
     margin-left: 2em;
     margin-right: 2em;
-}
-.bar i {
-    font-size: 1.3em;
     color: rgb(152, 152, 156);
+}
+.router-link-active {
+    color: rgb(55, 88, 247);
 }
 </style>
 
@@ -44,24 +44,25 @@
         <router-view class="container" />
         <div class="navbar">
             <div class="nav">
-                <div class="bar" @click="toTodolist">
-                    <label for="">
-                        <i
-                            class="fa fa-calendar-check-o"
-                            aria-hidden="true"
-                        ></i>
-                    </label>
-                </div>
-                <div class="bar" @click="toPomodoro">
-                    <label for="">
-                        <i class="fa fa-bullseye" aria-hidden="true"></i>
-                    </label>
-                </div>
-                <div class="bar" @click="toDing">
-                    <label for="">
-                        <i class="fa fa-cube" aria-hidden="true"></i>
-                    </label>
-                </div>
+                <!-- <label for="" class="bar" @click="toTodolist">
+                    <i class="fa fa-calendar-check-o" aria-hidden="true"></i>
+                </label>
+                <label for="" class="bar" @click="toPomodoro">
+                    <i class="fa fa-bullseye" aria-hidden="true"></i>
+                </label>
+
+                <label for="" class="bar" @click="toDing">
+                    <i class="fa fa-cube bar" aria-hidden="true"></i>
+                </label> -->
+                <router-link :to="'/todolist'" class="bar" tag="div">
+                    <i class="fa fa-calendar-check-o" style="font-size:1.2em" aria-hidden="true"></i>
+                </router-link>
+                <router-link :to="'/pomodoro'" class="bar" tag="div">
+                    <i class="fa fa-bullseye" style="font-size:1.2em" aria-hidden="true"></i>
+                </router-link>
+                <router-link :to="'/ding'" class="bar" tag="div">
+                    <i class="fa fa-clock-o" style="font-size:1.2em" aria-hidden="true"></i>
+                </router-link>
             </div>
         </div>
     </div>
@@ -70,46 +71,47 @@
 <script>
 export default {
     name: "app",
-    data() {
-        return {
-            old: null,
-            current: null,
-        };
-    },
-    mounted: function () {
-        this.current = document.getElementsByClassName("fa fa-cube")[0];
-    },
-    methods: {
-        navigateTo: function (url) {
-            this.$router.replace(url);
-        },
-        changeCurrent: function (e) {
-            this.old = this.current;
-            this.current = e.target;
-        },
-        toDing: function (e) {
-            // console.log(e)
-            this.changeCurrent(e);
-            this.$router.replace("/ding");
-        },
-        toPomodoro: function (e) {
-            this.changeCurrent(e);
-            this.$router.replace("/pomodoro");
-        },
-        toTodolist: function (e) {
-            this.changeCurrent(e);
-            this.$router.replace("/todolist");
-        },
-    },
-    watch: {
-        current: function () {
-            // console.log(this.current)
-            this.current.style.color = "rgb(55,88,247)";
-            if (this.old) {
-                this.old.style.color = "rgb(152, 152, 156)";
-            }
-        },
-    },
+    // data() {
+    //     return {
+    //         old: null,
+    //         current: null,
+    //     };
+    // },
+    // mounted: function () {
+    //     this.current = document.getElementsByClassName("fa fa-cube")[0];
+    // },
+    // methods: {
+    //     navigateTo: function (element, url) {
+    //         if (this.changeCurrent(element)) this.$router.push({ path: url });
+    //     },
+    //     changeCurrent: function (element) {
+    //         console.log(element);
+    //         console.log(this.current);
+    //         if (this.current == element) return false;
+    //         this.old = this.current;
+    //         this.current = element;
+    //         return true;
+    //     },
+    //     toDing: function (e) {
+    //         // console.log(e)
+    //         this.navigateTo(e.target, "/ding");
+    //     },
+    //     toPomodoro: function (e) {
+    //         this.navigateTo(e.target, "/pomodoro");
+    //     },
+    //     toTodolist: function (e) {
+    //         this.navigateTo(e.target, "/todolist");
+    //     },
+    // },
+    // watch: {
+    //     current: function () {
+    //         // console.log(this.current)
+    //         this.current.style.color = "rgb(55,88,247)";
+    //         if (this.old) {
+    //             this.old.style.color = "rgb(152, 152, 156)";
+    //         }
+    //     },
+    // },
 };
 </script>
 
