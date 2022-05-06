f8a7dea6afd0673b9c6fa185b8ee6ef3dbfdc553
Drinkwater-alt <2500598478@qq.com>
2022-05-06 11:33:06 +0800

    更新打卡页面item组件

--- src/views/DailyItem.vue
+++ src/views/DailyItem.vue
@@ -1,16 +1,35 @@
 <template>
-  <div style="display:flex;background-color:#ffffff;width:95vw;height:2.8em;">
+  <div style="display:flex;background-color:#ffffff;width:95vw;height:3.1em;">
     <!-- 图标 -->
     <svg
+      v-if="!info.finish"
+      @click="convertFinish(info.idx)"
       class="icon svg-icon"
       style="top:0;bottom:0;margin:auto;margin-left:2.5vw;margin-right:0vw;"
       aria-hidden="true"
     >
       <use :xlink:href="info.iconName" />
     </svg>
+    <svg
+      v-else
+      @click="convertFinish(info.idx)"
+      class="miniIcon svg-icon"
+      style="top:0;bottom:0;margin:auto;margin-left:2.5vw;margin-right:0vw;"
+      aria-hidden="true"
+    >
+      <use xlink:href="#icon-duigou" />
+    </svg>
 
-    <!-- 任务内容 TODO:字体大小自适应；字体样式-->
-    <p style="top:0;bottom:0;margin:auto;margin-left:2.5vw;font-size:15px">{{info.title}}</p>
+    <!-- 任务内容-->
+    <p
+      style="top:0;bottom:0;margin:auto;margin-left:2.7vw;font-size:15px;color:#000000"
+    >{{info.title}}</p>
+
+    <!-- 右侧信息 -->
+    <div style="display:block;margin-top:1vh;bottom:0;margin-right:4vw;">
+      <p style="font-size:15px;font-weight:520;color:#000000">{{info.days}}天</p>
+      <p style="font-size:10px">共坚持</p>
+    </div>
   </div>
 
   <!-- <div class="layout" style="width:95vw;">
@@ -27,13 +46,17 @@ export default {
   methods: {
 
   },
-  props: ['info'],
+  props: ['info'],// { iconName: "#icon-youxi", title: "Play", finish: true, },
   data: function () {
     return {
       //   iconName: "#icon-yishu",
       // title: "你好"
     }
-  },
+  }, methods: {
+    convertFinish(idx) {
+      this.$parent.convertFinish(idx);
+    }
+  }
 };
 
 </script>
--- src/views/Ding.vue
+++ src/views/Ding.vue
@@ -4,7 +4,11 @@
       <span>打卡</span>
     </div>
     <div class="datebar">
-      <div class="date">
+      <div class="date" v-for="dayItem in dayItems ">
+        <span>{{dayItem.dayText}}</span>
+        <span class="day">{{dayItem.dayNum}}</span>
+      </div>
+      <!-- <div class="date">
         <span>一</span>
         <span class="day">25</span>
       </div>
@@ -33,12 +37,12 @@
         <div class="circle">
           <span>今天</span>
         </div>
-      </div>
+      </div>-->
     </div>
 
     <div style="width:10vw;height:10vh;">
       <div v-for="item in items">
-        <dailyItem :info = "item" ></dailyItem>
+        <dailyItem :info="item"></dailyItem>
       </div>
     </div>
     <div class="cardsbar"></div>
@@ -54,14 +58,56 @@ export default {
   components: {
     dailyItem
   },
+  mounted: function () {
+    for (var i = 6; i >= 0; i--) {
+      this.dayItems.push(this.getDayInfo(i));
+    }
+    console.log(this.dayItems);
+  },
   data: function () {
     return {
       items: [
-        { iconName: "#icon-youxi", title: "Play" },
-        { iconName: "#icon-qingjie", title: "洗脸" },
-      ]
+        { idx: 0, iconName: "#icon-youxi", title: "Play", finish: true, days: 0, },
+        { idx: 1, iconName: "#icon-qingjie", title: "洗脸", finish: false, days: 1 },
+      ],
+      year: 2022,
+      month: 5,
+      day: 6,
+      days: [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+      dayItems: []
     }
-  },
+  }, methods: {
+    convertFinish(idx) {
+      this.items[idx].finish = !this.items[idx].finish;
+    },
+    getDayText(year, month, day) {
+      // 返回对应日期是星期几
+      return "一";
+    },
+    getDayInfo(idx) {
+      var info = {};
+      var year = this.year; var month = this.month; var day = this.day;
+      day -= idx;
+      if (day <= 0) {
+        month -= 1;
+      }
+      if (month <= 0) {
+        year -= 1;
+      }
+
+      if (month <= 0) {
+        month = 12 - month;
+      }
+
+      if (day <= 0) {
+        day = this.days[month - 1] - day;
+      }
+      info.dayNum = day;
+      info.dayText = this.getDayText(year, month, day);
+
+      return info;
+    }
+  }
 };
 </script>
 
