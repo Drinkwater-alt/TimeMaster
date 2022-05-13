a112df29e85669f8c4fa4793e1bfec84a6edec7d
Ethan-Yuanxy <947253705@qq.com>
2022-05-13 11:48:53 +0800

    完成todolist基本样式布局

--- src/components/AddButton.vue
+++ src/components/AddButton.vue
@@ -11,22 +11,18 @@ export default {};
 </script>
 
 <style scoped>
-.container {
-}
 .spherical {
     border-radius: 50%;
     background-color: rgb(56, 87, 245);
     width: 7rem;
     height: 7rem;
-    display: flex;
-    align-items: center;
-    justify-content: center;
     box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.2);
-
+    text-align: center;
 }
 span {
     color: #fff;
     font-size: 6rem;
     font-weight: 300;
+    line-height: 5.8rem;
 }
 </style>
--- src/views/ToDoList.vue
+++ src/views/ToDoList.vue
@@ -1,15 +1,92 @@
 <template>
-  <div>
-      todolist
-  </div>
+    <div>
+        <add-button class="addbutton"></add-button>
+        <div class="header">
+            <span>TODOLIST</span>
+            <label for="">
+                <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
+            </label>
+        </div>
+        <div class="body">
+            <Card class="completing">
+                <List v-bind:items="plan">
+                    <template v-slot:item="{ item }">
+                        <span style="color: #555">{{ item.content }}</span>
+                        <span>{{ item.date }}</span>
+                    </template>
+                </List>
+            </Card>
+            <Card class="completed">
+                <List v-bind:items="completed">
+                    <template v-slot:item="{ item }">
+                        <span style="color: #555">{{ item.content }}</span>
+                        <span>{{ item.date }}</span>
+                    </template>
+                </List>
+            </Card>
+        </div>
+    </div>
 </template>
 
 <script>
+import AddButton from "../components/AddButton.vue";
+import List from "../components/List.vue";
+import Card from "../components/Card.vue";
 export default {
-
-}
+    components: { AddButton, List, Card },
+    data() {
+        return {
+            plan: [
+                {
+                    content: "窟窿",
+                    date: "2022-5-13",
+                },
+                {
+                    content: "123",
+                    date: "2022-5-13",
+                },
+            ],
+            completed:[
+                {
+                    content: "窟窿",
+                    date: "2022-5-13",
+                },
+                {
+                    content: "123",
+                    date: "2022-5-13",
+                },
+            ]
+        };
+    },
+};
 </script>
 
-<style>
-
+<style scoped>
+.addbutton {
+    position: absolute;
+    right: 0.5rem;
+    bottom: 0.3rem;
+}
+.header {
+    color: #222;
+    padding: 1rem 2rem;
+    display: flex;
+    justify-content: space-between;
+}
+.header i {
+    font-size: 3rem;
+    /* line-height: .5rem; */
+}
+.body {
+    padding: 0.5rem 2rem;
+}
+.completed,
+.completing {
+    background-color: white;
+    border-radius: 1rem;
+    border: none;
+}
+.completed {
+    margin-top: 2rem;
+}
 </style>
